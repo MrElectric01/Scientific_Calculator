a3598b8b9115c8dde1d3f6b226a3347cbcbe0f68
MrElectric01/Christian+Conato <christianconato123@gmail.com>
2023-10-26 17:08:40 +0200

    Differenza e rapporto

--- CalcolatriceScientifica.java
+++ CalcolatriceScientifica.java
@@ -3,8 +3,15 @@ package Scientific_Calculator;
 public class CalcolatriceScientifica {
 
     public static void main(String[] args) {
-        System.out.println("Ciao sono il grande MrElectric da ben 2800 iscritti su youtube");
-        System.out.println("Bellaa sono Giosu");
+        
+    }
+    
+    public double differenza(double a, double b){
+        return a-b;
+    }
+    
+    public double rapporto(double a, double b){
+        return a/b;
     }
 }
 

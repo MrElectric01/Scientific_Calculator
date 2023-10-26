cb52b24662f84a36c7911dc91e49a1f7c4c32901
Ilguardianonotturno <tanatome08012001@gmail.com>
2023-10-26 14:27:40 +0200

    we

--- CalcolatriceScientifica.java
+++ CalcolatriceScientifica.java
@@ -3,7 +3,8 @@ package Scientific_Calculator;
 public class CalcolatriceScientifica {
 
     public static void main(String[] args) {
-        System.out.println("Bella zii!");
+        System.out.println("puzzo e sono nunzio");
+        
     }
 }
 

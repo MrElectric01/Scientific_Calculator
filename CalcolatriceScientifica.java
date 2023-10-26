da90a6620f61fab8588f6f7c1bfbefa9eced2880
GiosuCiaravola <giosueciaravola@gmail.com>
2023-10-26 15:50:02 +0200

    Nunzio non ce la fa

--- CalcolatriceScientifica.java
+++ CalcolatriceScientifica.java
@@ -4,6 +4,8 @@ public class CalcolatriceScientifica {
 
     public static void main(String[] args) {
         System.out.println("Bella zii!");
+        System.out.println("aggiunta pazza");
+        System.out.println("si sono giosu");
     }
 }
 

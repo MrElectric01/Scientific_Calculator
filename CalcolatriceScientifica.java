193bf7641ffd0ed1fec0951c709fbef6e242c9db
Ilguardianonotturno <tanatome08012001@gmail.com>
2023-10-26 17:24:23 +0200

    risultati come stinghe

--- CalcolatriceScientifica.java
+++ CalcolatriceScientifica.java
@@ -5,21 +5,27 @@ public class CalcolatriceScientifica {
     public static void main(String[] args) {
         
     }
+
+
+
     
-    public double differenza(double a, double b){
-        return a-b;
+    public String differenza(double a, double b){
+        return Double.toString(a-b);
     }
     
-    public double rapporto(double a, double b){
-        return a/b;
+    public String rapporto(double a, double b){
+        if(b==0){
+            return(" Impossibile dividere per zero");
+        }
+        return Double.toString(a/b);
     }
 
-    public double somma(double a, double b){
-        return a+b;
+    public String somma(double a, double b){
+        return Double.toString(a+b);
     }
 
-    public double prodotto(double a, double b){
-        return a*b;
+    public String prodotto(double a, double b){
+        return Double.toString(a*b);
     }
 }
 
